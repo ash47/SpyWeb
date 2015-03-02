398fbf3dfb5754c7943acf908a2626e5bf1c9e2e
ash47 <ash.fourtyseven@gmail.com>
2015-03-02 17:33:22 +1100

    Implemented stat calculation code

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@
 // Teams
 var TEAM_GOOD = 'good';
 var TEAM_BAD = 'bad';
+var TEAM_VEHICLE = 'vehicle';
 
 // Directions
 var DIR_LEFT = 'left';
@@ -29,6 +30,9 @@ function Character(name, data) {
 
     // The team they belong to
     this.team = data.team;
+
+    // If they are a vehicle
+    if(data.vehicle) this.vehicle = data.vehicle;
 
     /* The moves that are allowed
         {
@@ -60,6 +64,43 @@ Character.prototype.getName = function() {
     return this.name;
 }
 
+// Checks if this character can move in the given direction
+Character.prototype.canMove = function(direction) {
+    return this.moves[direction] != null;
+}
+
+// Returns true if this is a vehicle
+Character.prototype.isVehicle = function() {
+    return this.vehicle != null;
+}
+
+// Sets the type of vehicle this is
+Character.prototype.setVehicle = function(sort) {
+    this.vehicle = sort;
+}
+
+/* Create vehicles */
+var VEHICLE_BOAT = new Character('boat', {
+    moves: {},
+    team: TEAM_VEHICLE,
+    bounty: 0,
+    vehicle: 'boat'
+});
+
+var VEHICLE_CAR = new Character('car', {
+    moves: {},
+    team: TEAM_VEHICLE,
+    bounty: 0,
+    vehicle: 'car'
+});
+
+var VEHICLE_PLANE = new Character('plane', {
+    moves: {},
+    team: TEAM_VEHICLE,
+    bounty: 0,
+    vehicle: 'plane'
+});
+
 /*
     Board class
 */
@@ -81,6 +122,11 @@ Board.prototype.getCharacter = function(x, y) {
             return this.characters[3*y + x];
         }
     }
+
+    // Check specical spaces
+    if(x == 3 && y == 2) return VEHICLE_BOAT;
+    if(x == -1 && y == 0) return VEHICLE_CAR;
+    if(x == 2 && y == -1) return VEHICLE_PLANE;
 
     // Currently null
     return null;
@@ -111,9 +157,13 @@ Board.prototype.display = function(div) {
     // Create a container for each of our characters
     for(var y=0; y<=2; ++y) {
         for(var x=0; x<=2; ++x) {
-            var character = this.getCharacter(x, y);
+            var charDisplay = $('<div>').attr('class', 'charDisplay')
 
-            var charDisplay = $('<div>').attr('class', 'charDisplay').css('background-image', 'url("' + character.getImage(false) + '")').css('background-size', '128px 128px');
+            var character = this.getCharacter(x, y);
+            if(character) {
+                charDisplay.css('background-image', 'url("' + character.getImage(false) + '")').css('background-size', '128px 128px');
+            }
+
             ourCon.append(charDisplay);
         }
 
@@ -135,7 +185,71 @@ Board.prototype.calcStats = function() {
         for(var x=0; x<=2; ++x) {
             var character = this.getCharacter(x, y);
 
+            if(character != null && character.canMove(DIR_RIGHT)) {
+                var moveCharacter = this.getCharacter(x+1, y);
 
+                if(moveCharacter != null) {
+                    if(moveCharacter.isVehicle()) {
+                        vehicle++;
+                        total++;
+                        unique++;
+                    } else {
+                        total++;
+                        unique++;
+                    }
+                }
+            }
+
+            if(character != null && character.canMove(DIR_DOWN)) {
+                var moveCharacter = this.getCharacter(x, y+1);
+
+                if(moveCharacter != null) {
+                    if(moveCharacter.isVehicle()) {
+                        vehicle++;
+                        total++;
+                        unique++;
+                    } else {
+                        total++;
+                        unique++;
+                    }
+                }
+            }
+
+            if(character != null && character.canMove(DIR_LEFT)) {
+                var moveCharacter = this.getCharacter(x-1, y);
+
+                if(moveCharacter != null) {
+                    if(moveCharacter.isVehicle()) {
+                        vehicle++;
+                        total++;
+                        unique++;
+                    } else {
+                        total++;
+
+                        if(!moveCharacter.canMove(DIR_RIGHT)) {
+                            unique++;
+                        }
+                    }
+                }
+            }
+
+            if(character != null && character.canMove(DIR_UP)) {
+                var moveCharacter = this.getCharacter(x, y-1);
+
+                if(moveCharacter != null) {
+                    if(moveCharacter.isVehicle()) {
+                        vehicle++;
+                        total++;
+                        unique++;
+                    } else {
+                        total++;
+
+                        if(!moveCharacter.canMove(DIR_DOWN)) {
+                            unique++;
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -181,8 +295,36 @@ $(document).ready(function(){
     // We can cleanup the characterData now
     delete characterData;
 
-    // Setup a test board
+    /*function applyLoop(pool, board, slot, charNumber) {
+        if(board == null) {
+            board = new Board();
+        }
+
+        console.log(slot + ' ' + charNumber);
+
+        if(charNumber < pool.length) {
+            charNumber++;
+        } else {
+            slot++;
+            charNumber = 0;
+        }
+
+        if(slot > 8) {
+            return;
+        }
+
+        applyLoop(pool, board, slot, charNumber);
+
+        //for(var i=0; i<pool.length; ++i) {
+        //    console.log(i);
+        //}
+    }
+
     var board = new Board();
+    applyLoop(goodCharacters, board, 0, 0);*/
+
+    // Setup a test board
+    /*var board = new Board();
 
     for(var x=0; x<=2; ++x) {
         for(var y=0; y<=2; ++y) {
@@ -190,7 +332,9 @@ $(document).ready(function(){
         }
     }
 
-    board.display($('#content'));
+    board.setCharacter(1, 1, null);
 
-    console.log(board.calcStats());
+    board.display($('#content'));*/
+
+    //console.log(board.calcStats());
 });
